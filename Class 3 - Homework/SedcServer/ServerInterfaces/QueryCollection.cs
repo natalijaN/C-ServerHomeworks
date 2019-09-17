808d96185376be8370e94593a3075f167fddb56a
Natalija <natalija_nikolova@yahoo.com>
2019-09-17 20:53:24 +0200

    Added Class 3 Homework

---
+++ Class 3 - Homework/SedcServer/ServerInterfaces/QueryCollection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ServerInterfaces
+{
+    public class QueryCollection
+    {
+        private readonly List<KeyValuePair<string, string>> data = new List<KeyValuePair<string, string>>();
+
+        public QueryCollection(List<KeyValuePair<string, string>> initialData)
+        {
+            data = initialData;
+        }
+
+        public static QueryCollection Empty
+        {
+            get
+            {
+                return new QueryCollection(new List<KeyValuePair<string, string>>());
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var kvp in data)
+            {
+                sb.AppendLine($"{kvp.Key}={kvp.Value}");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Class 3 - Homework/SedcServer/ServerRunner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using ServerCore;
+using ServerInterfaces;
+
+namespace ServerRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new WebServer();
+            server.Start();
+        }
+    }
+}
